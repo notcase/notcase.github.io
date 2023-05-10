b9182767fc39fdadb392f586d6b0b7068f57c25c
Not Case <128095951+notcase@users.noreply.github.com>
2023-05-10 12:23:55 -0400

    completed project

---
+++ Cameleon-final-project/app.js
@@ -0,0 +1,8 @@
+// function lightMode() {
+//   document.body.classList.toggle("lightMode");
+// }
+var lightMode = document.querySelector(".lightmodebtn");
+
+function toggleLightMode() {
+  document.body.classList.toggle("lightMode");
+}
